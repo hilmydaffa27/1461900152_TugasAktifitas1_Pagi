ee9e94a3e10485cea83c29c16bc2c56bf3945454
hilmydaffa27 <jameshilmy@gmail.com>
2021-05-22 14:14:31 +0700

    prak1

---
+++ resources/views/artikel0152blade.php
@@ -0,0 +1,64 @@
+<<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Web Hilmy Daffa</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="/artikel">artikel</a>
+        <a class="nav-link" href="/contact us">contact us</a>
+        
+      </div>
+    </div>
+  </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+<div class="col-10">
+<h1 class="mt-2">
+<h1 class="mt-2">Kelebihan Framework</h1>
+<p class="left">1. Lebih cepat dan efisien
+Jika Anda mengerjakan proyek besar, maka penggunaan framework dapat membantu mempercepat proses pengembangan. Pada umumnya, framework memiliki beragam fungsi dan plugin yang bisa Anda manfaatkan. Dengan kerangka kerja ini, maka proses pengembangan proyek jauh lebih cepat daripada Anda harus menulis kode dari awal. Selain itu, Anda juga tidak perlu menulis berulang-ulang untuk kode yang bersifat repetitif.<p>
+<p class="left">2. Menghemat biaya
+Sebagian besar kerangka populer bersifat open source dan gratis untuk digunakan. Biaya yang harus dikeluarkan oleh client Anda juga akan menjadi lebih kecil karena proses pengerjaan yang lebih simple dan lebih cepat.<p>
+<p class="left">3. Memperhatikan faktor keamanan
+Framework populer telah banyak digunakan oleh banyak pengembang, dan kemungkinan adanya masalah keamanan atau pun bug telah diperbaiki. Selain itu, framework biasanya juga memiliki komunitas besar yang dapat berperan sebagai penguji jangka panjang. Setiap kali pengguna menemukan celah keamanan, maka mereka dapat memberi tahu tim untuk segera memperbaikinya.<p>
+<h1 class="mt-2">Kekurangan Framework</h1>
+<p class="left">1. Kurangnya pemahaman bahasa pemrograman
+Jika Anda bekerja menggunakan kerangka kerja dan hanya mengetahui sedikit tentang bahasa pemrograman yang digunakan dibaliknya, maka Anda hanya mempelajari mengenai kerangka tersebut. Sehingga pemahaman mengenai  bahasa pemrograman menjadi tidak berkembang.<p>
+<p class="left">2. Memiliki batasan
+Dalam penggunaannya, kerangka ini juga memiliki beberapa batasan yang tidak dapat Anda modifikasi. Sehingga Anda harus bekerja sesuai dengan standar yang digunakan di dalamnya. Oleh karena itu, ketika mengembangkan sebuah aplikasi, Anda harus menggunakan kerangka kerja yang sesuai dengan kebutuhan Anda.<p>
+<p class="left"><p>
+</div>
+</div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
